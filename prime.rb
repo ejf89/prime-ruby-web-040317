1be449d1f944664d5f2bfdc39bccfb90961200b0
Eric Farber <eric.j.farber@gmail.com>
2017-03-25 19:40:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+
+def prime?(x)
+
+return false if x <= 1
+
+x = Math.sqrt(x)
+
+    (1..x).to_a.each do |number|
+        puts number
+        if x % number == 0
+            return false
+        elsif x % number != 0
+            return true
+        end
+    end
+end
+
+prime?(40)
